4eb929bf22382837c7123aa998306e3289080ba5
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-07-24 20:34:21 -0400

    Integrate subscriptions into signals

--- src/sigsub/signal.cljs
+++ src/sigsub/signal.cljs
@@ -2,6 +2,9 @@
 
 (def capturing? false)
 (def signal-being-captured (atom nil))
+(def active-signals {})
+(def default-signal-f nil)
+(def registered-signals {})
 
 (declare run-with-deref-capture)
 (declare notify-deref-watcher)
@@ -26,15 +29,26 @@
 (defprotocol IDisposable
              (-dispose [this]))
 
-(deftype Derived [f ^:mutable current-value ^:mutable parents ^:mutable children]
+(defprotocol ISubscribable
+             (-subscribe [this])
+             (-unsubscribe [this]))
+
+(defprotocol IDirty
+             (-mark-dirty [this]))
+
+(deftype Derived [path f ^:mutable current-value ^:mutable dirty? ^:mutable subscriptions
+                  ^:mutable parents ^:mutable children]
          IDeref
          (-deref [this]
                  (notify-deref-watcher this)
+                 (when dirty?
+                       (-refresh this))
                  current-value)
          IRefreshable
          (-refresh [this]
                    (-unbind-parents this)
-                   (run-with-deref-capture this))
+                   (run-with-deref-capture this)
+                   (set! dirty? false))
          IRunnable
          (-run [this]
                (let [value (f)]
@@ -45,12 +59,13 @@
          (-add-child [this child]
                      (set! children (conj children child)))
          (-remove-child [this child]
-                        (set! children (disj children child))
-                        (if (empty? children)
-                          (-dispose this)))
+                        (set! children (disj children child)))
          (-notify-children [this]
                            (doseq [child children]
-                                  (-refresh child)))
+                                  (-mark-dirty child)))
+         IDirty
+         (-mark-dirty [this]
+                      (set! dirty? true))
          IChild
          (-add-parent [this parent]
                       (set! parents (conj parents parent)))
@@ -59,14 +74,21 @@
          (-unbind-parents [this]
                           (doseq [parent parents]
                                  (unbind-dependency parent this)))
-
+         ISubscribable
+         (-subscribe [this]
+                     (set! subscriptions (inc subscriptions)))
+         (-unsubscribe [this]
+                       (set! subscriptions (dec subscriptions))
+                       (when (= subscriptions 0)
+                             (-dispose this)))
          IDisposable
          (-dispose [this]
                    (doseq [parent parents]
                           (-remove-child parent this))
                    (set! parents nil)
                    (set! children nil)
-                   (set! current-value nil)))
+                   (set! current-value nil)
+                   (set! active-signals (dissoc active-signals path))))
 
 (deftype Base [parent ^:mutable children]
          IDeref
@@ -90,9 +112,9 @@
                              (-notify-children base))))
            base))
 
-(defn make-derived [f]
-      (let [signal (Derived. f nil #{} #{})]
-           (-refresh signal)
+(defn make-derived [path f]
+      (let [signal (Derived. path f nil true 1 #{} #{})]
+           (set! active-signals (assoc active-signals path signal))
            signal))
 
 (defn run-with-deref-capture [signal]
@@ -118,4 +140,39 @@
 (defn notify-deref-watcher [signal]
       (if capturing?
         (bind-dependency signal @signal-being-captured)))
+
+; subscriptions
+
+(defn register-signal [path f]
+      (set! registered-signals (assoc-in registered-signals path f)))
+
+(defn register-default-signal-f [f]
+      (set! default-signal-f f))
+
+(defn get-in-atom-signal-f [atom]
+      (let [base (make-base atom)]
+           (fn [path]
+               (get-in @base path))))
+
+(defn- path->derived-signal-f [path]
+       (loop [node registered-signals path path]
+             (cond (map? node)
+                   (recur (node (first path)) (rest path))
+                   (ifn? node)
+                   (node path))))
+
+(defn path->signal-f [path]
+      (if-let [f (path->derived-signal-f path)]
+              f
+              #(default-signal-f path)))
+
+(defn subscribe [path]
+      (if-let [signal (active-signals path)]
+              (do (-subscribe signal)
+                  signal)
+              (make-derived path (path->signal-f path))))
+
+(defn unsubscribe [signal]
+      (-unsubscribe signal))
+
 
--- test/sigsub/core.cljs
+++ test/sigsub/core.cljs
@@ -9,21 +9,37 @@
               (reset! a 2)
               (is (= @b 2))))
 
-(deftest derived-signal
-         (let [a (atom 1)
-               b (signal/make-base a)
-               c (signal/make-derived #(+ @b 1))
-               d (signal/make-derived #(+ @c 1))
-               e (signal/make-derived #(if (= @b 1) @c @d))]
-              (is (= @c 2))
-              (is (= @d 3))
-              (is (= @e 2))
-              (reset! a 2)
-              (is (= @c 3))
-              (is (= @d 4))
-              (is (= @e 4))))
+(deftest default-derived-signal
+         (set! signal/active-signals {})
+         (let [a (atom {:test {:value 1}})]
+              (signal/register-default-signal-f (signal/get-in-atom-signal-f a))
+              (let [c (signal/subscribe [:test :value])]
+                   (is (= @c 1))
+                   (swap! a assoc-in [:test :value] 2)
+                   (is (= @c 2)))))
 
-(deftest derived-signal-computed-when-neccesary
+(deftest registered-derived-signal
+         (set! signal/active-signals {})
+         (set! signal/registered-signals {})
+         (let [a (atom {:test 1})]
+              (signal/register-default-signal-f (signal/get-in-atom-signal-f a))
+              (signal/register-signal
+                [:b]
+                (fn [] (let [a (signal/subscribe [:test])]
+                            #(+ @a 1))))
+              (signal/register-signal
+                [:c]
+                (fn [] (let [b (signal/subscribe [:b])]
+                            #(+ @b 1))))
+              (let [b (signal/subscribe [:b])
+                    c (signal/subscribe [:c])]
+                   (is (= @b 2))
+                   (is (= @c 3))
+                   (reset! a {:test 2})
+                   (is (= @b 3))
+                   (is (= @c 4)))))
+
+#_(deftest derived-signal-computed-when-neccesary
          (let [computed (atom 0)
                a (atom {:important 1 :useless 2})
                b (signal/make-base a)
@@ -39,7 +55,7 @@
               (is (= @d 2))
               (is (= @computed 1))))
 
-(deftest derived-signal-parent-child-maintained
+#_(deftest derived-signal-parent-child-maintained
          (let [a (atom 1)
                b (signal/make-base a)
                c (signal/make-derived #(+ @b 1))
@@ -51,5 +67,27 @@
               (is (= @d 3))
               (is (= (.-parents d) #{b}))
               (is (= (.-children c) #{}))))
+
+;(set! signal/active-signals {})
+;(set! signal/registered-signals {})
+
+;(keys signal/registered-signals)
+#_(let [a (atom {:test 1})]
+     (signal/register-default-signal-f (signal/get-in-atom-signal-f a))
+     (signal/register-signal
+       [:b]
+       (fn [] (let [a (signal/subscribe [:test])]
+                   #(+ @a 1))))
+     (signal/register-signal
+       [:c]
+       (fn [] (let [b (signal/subscribe [:b])]
+                   #(+ @b 1)))))
+
+;(def b (signal/subscribe [:b]))
+;(.-f b)
+;(def c (signal/subscribe [:c]))
+
+;(+ 1 1)
+;@b
 
 (run-tests)
